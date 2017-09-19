49591ea198da53dab844fd47df1d20f6aee4f7fd
Aayush Sharma <aayushs96@gmail.com>
2017-09-19 12:05:15 -0500

    dynamic resource

--- questions/DynamicHist/server.py
+++ questions/DynamicHist/server.py
@@ -5,15 +5,18 @@ def generate(data):
 
     a = random.randint(20, 100)
     histo = [random.randint(1, 100) for i in range(a)]
-    plt.hist(histo)
+    #plt.hist(histo)
     #data["params"]["a"] = a
  
     data["correct_answers"]["a"] = a
 
     return data
+    
+
+
 def file(data):
     if data['filename']=='figure.png':
-        plt.plot(histo)
+        plt.hist(histo)
         buf = io.BytesIO()
         plt.savefig(buf,format='png')
         return buf
