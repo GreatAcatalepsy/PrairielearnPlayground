35778679665bcaba0ba2d076a37e947bfc87c80b
Connie Fan <cfan12@illinois.edu>
2017-09-20 20:16:46 -0500

    broken static image question

--- questions/stat100ch3_staticHist/server.py
+++ questions/stat100ch3_staticHist/server.py
@@ -1,11 +1,37 @@
 import random
+import json
 
 def generate(data):
 
-    a = random.randint(1, 10)
-    data["params"]["a"] = a
+    num_elements = 5   #  data["options"]["num_elements"] 
+    bucket_size = 20
+    samples = random.sample(range(10, 40), num_elements)
 
-    Interval = 100/a 
-    data["correct_answers"]["Interval"] = Interval
+    ## what follows isn't the most pythonic way of doing this, but it is simpler for 
+    ## those that aren't familiar with functional programming
+    sample_array = []
+    for i in range(len(samples)):
+        bucket = "%s - %s" % (i*bucket_size, (i+1)*bucket_size)
+        sample_array.append({'value': samples[i], 'name': bucket, 'index': i})
+        data["correct_answers"]["input%s" % (i)] = samples[i]
+
+    data["params"]["samples"] = sample_array
+##    data["params"]["sample0"] = samples[0]
+##    data["params"]["sample1"] = samples[1]
+
+    ## temporary code to see what is going on in the 'data' dictionary
+    data["params"]["data"] = json.dumps(data)
+
+## This causes an error
+##    print(json.dumps(data))
 
     return data
+
+
+## temporary hack to show that we can dump the 'data' dictionary after student submission.
+## def grade(data):
+##     data["score"] = 1
+## 
+##     data["feedback"]["data"] = json.dumps(data)
+## 
+##     return data
